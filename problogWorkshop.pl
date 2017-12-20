7f5717822fba2720965718657e4be06d7a51a195
prakshithreddy <31597728+prakshithreddy@users.noreply.github.com>
2017-12-19 19:52:45 -0500

    Included problog program into the repo..

--- problogWorkshop.pl
+++ problogWorkshop.pl
@@ -1,1 +1,18 @@
+%%%%Load data from the example input files below this(input1.pl,input2.pl,..)
+person(rakshith).
+person(dinesh).
+person(proffodor).
+person(prashanth).
 
+attr(weather).
+attr(timeofWorkshop).
+attr(topic).
+attr(host).
+%%%%%%%%
+
+series:- person(P),attends(P),sa(P).
+0.501::sa(P):-person(P).
+attends(P):- person(P),attr(A),at(P,A).
+0.3::at(P,A):-person(P),attr(A).
+
+query(series).
